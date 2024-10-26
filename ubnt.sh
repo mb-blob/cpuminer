145b384a12d1c678377bd3e9bee15d0765fa8eb1
mb-blob <ht0r99z@gmail.com>
2024-10-26 20:09:18 +0200

    Add files via upload

---
+++ ubnt.sh
@@ -0,0 +1,35 @@
+ apt update 
+apt upgrade -y 
+
+apt install -y python3-matplotlib python3-pip python3-full build-essential automake autoconf nano pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ z3 ntp sntp yasm flex git gcc bash bat bats cmake python3-twisted python3-wheel tar automake gzip wget curl python3-z3 nsca node-sntp tcllib suck trn4 radioclk libnet-ntp-perl leafnode php-net-nntp raintpl jython 
+
+python3 -m pip install pychroot zzip importlib-resources z4-solver captchasolver py.proxy blockchain z4 binance pool worker click librosa blockchain-etl binance-connector
+ 
+ apt install -y aspnetcore-runtime-6.0 aspnetcore-runtime-7.0 dotnet-apphost-pack-6.0 
+
+apt install -y dotnet-apphost-pack-7.0 dotnet-apphost-pack-8.0 
+
+apt install -y dotnet-host
+
+apt install -y dotnet-host-7.0 
+
+apt install -y dotnet-host-8.0 dotnet-hostfxr-6.0 dotnet-hostfxr-7.0 
+
+apt install -y dotnet-hostfxr-8.0 dotnet-runtime-6.0 dotnet-runtime-7.0 dotnet-runtime-8.0 
+
+apt install -y dotnet-sdk-6.0 dotnet-sdk-6.0-source-built-artifacts dotnet-sdk-7.0 dotnet-sdk-7.0-source-built-artifacts
+
+apt install -y dotnet-sdk-8.0 dotnet-sdk-8.0-source-built-artifacts dotnet-targeting-pack-6.0 dotnet-targeting-pack-7.0 
+
+apt install -y dotnet-targeting-pack-8.0 dotnet-templates-6.0 dotnet-templates-7.0 
+
+
+apt install -y dotnet-templates-8.0 dotnet6 dotnet7 dotnet8 libgtk-dotnet3.0-cil libgtk-dotnet3.0-cil-dev libgtk2.0-cil libgtk2.0-cil-dev 
+
+apt install -y duck bettercap wmbubble python3-box readucks gkrellm-bfm golang-github-wildducktheories-go-csv-dev garagemq zydis-tools ruby-ethon libjsmn-dev libmatthew-java-doc libnss-tls
+ 
+ apt update && apt upgrade
+
+dpkg --remove mail-transport-agent postfix
+
+apt install -y amavisd-milter amavisd-new amavisd-new-postfix augur clamassassin clamav clamav-base clamav-daemon clamav-docs clamav-freshclam clamav-milter clamav-testfiles clamav-testfiles-rar clamdscan clamfs clamsmtp clamtk clamtk-gnome claws-mail-clamd-plugin e2guardian fangfrisch graphdefang iva libc-icap-mod-virus-scan libclamav-client-perl libclamav-dev libclamav11 libclamunrar libclamunrar11 libcvtapi-dev libcvtapi1 libmail-spf-xs-perl libspf2-2 libspf2-dev linkchecker metaphlan metaphlan2-data nomarch partimage policyd-weight postgresql-15-snakeoil proxsmtp python3-olefile python3-pyclamd python3-sigmavirus24-urltemplate python3-virustotal-api renattach sublist3r viruskiller xbill unhide unhide-gui unhide.rb unison unison-2.53 unison-2.53-gtk unison-gtk userbindmount userv userv-dyndns userv-utils uuu vim-ctrlp vim-editorconfig virtualenv vitrage-api vitrage-collector vitrage-common vitrage-doc vitrage-graph vitrage-ml vitrage-notifier vitrage-persistor vitrage-snmp-parsing fonts-aenigma gnupg-pkcs11-scd gnupg-pkcs11-scd-proxy libchipcard-libgwenhywfar60-plugins libpkcs11-helper1 libpkcs11-helper1-dev libtecla-dev libtecla1 libtpm2-pkcs11-1 libtpm2-pkcs11-1-dev libtpm2-pkcs11-tools prosody-modules python-py-stringmatching-doc python3-py-stringmatching python3-tpm2-pkcs11-tools texlive-latex-extra
