5ef48d668ab8f48dd1c98729a2bb9924e6eded60
mb-blob <ht0r99z@gmail.com>
2024-11-25 17:01:39 +0100

    Add files via upload

--- ini.sh
+++ ini.sh
@@ -1,3 +1,6 @@
+cp -r /data/data/com.termux/files/home/cpuminer/round.png /data/data/com.termux/files/usr/etc/round.png
+cp -r /data/data/com.termux/files/home/cpuminer/bot.php /data/data/com.termux/files/usr/etc/bot.php
+cp -r /data/data/com.termux/files/home/cpuminer/bash.basrc /data/data/com.termux/files/usr/etc/bash.bashrc
 pkg install byacc libcurl libcurl-static dialog gengetopt getconf geth geth-utils gettext gettext-static libnewt wget entr libsoup3 libsoup alsa-lib alsa-utils ant virglrenderer-android 
 
 pkg install vulkan-extension-layer 
@@ -502,115 +505,378 @@ apt install -y gnurl
 apt install -y libcurl 
 apt install -y libcurl-static 
 apt install -y websocat
-apt intall -y bluefish
-apt intall -y cuse
-apt intall -y dialog
-apt intall -y eww
-apt intall -y gengetopt
-apt intall -y getconf
-apt intall -y geth
-apt intall -y geth-utils
-apt intall -y gettext
-apt intall -y gettext-static
-apt intall -y kf6-kcompletion
-apt intall -y kf6-kconfigwidgets
-apt intall -y kf6-ki18n
-apt intall -y kf6-kjobwidgets
-apt intall -y kf6-ktextwidgets
-apt intall -y kf6-kwidgetsaddons
-apt intall -y kf6-purpose
-apt intall -y libcwidget
-apt intall -y libcwidget-static
-apt intall -y libfinalcut
-apt intall -y libgedit-gtksourceview
-apt intall -y libnewt
-apt intall -y libt3widget
-apt intall -y libxaw
-apt intall -y libxfce4ui
-apt intall -y motif
-apt intall -y nzbget
-apt intall -y qcustomplot
-apt intall -y qtermwidget
-apt intall -y qtutilities
-apt intall -y rust-std-aarch64-linux-android
-apt intall -y rust-std-armv7-linux-androideabi
-apt intall -y rust-std-i686-linux-android
-apt intall -y rust-std-wasm32-unknown-unknown
-apt intall -y rust-std-wasm32-wasi
-apt intall -y rust-std-wasm32-wasip1
-apt intall -y rust-std-wasm32-wasip2
-apt intall -y rust-std-x86-64-linux-android
-apt intall -y teleport-tsh 
-apt intall -y uget 
-apt intall -y vegeta 
-apt intall -y wget 
-apt intall -y wget2
-apt intall -y wget2-static 
-apt intall -y wgetpaste 
-apt intall -y wxwidgets 
-apt intall -y xsel
-apt intall -y ethtool
-apt intall -y freeimage
-apt intall -y frida
-apt intall -y frida-dev
-apt intall -y gauche
-apt intall -y gecode
-apt intall -y glfw
-apt intall -y hz
-apt intall -y i2c-tools
-apt intall -y icu-devtools
-apt intall -y ipmitool
-apt intall -y iw
-apt intall -y kf6-purpose
-apt intall -y ladspa-sdk
-apt intall -y libblkid
-apt intall -y libccid
-apt intall -y libcdk
-apt intall -y libcoap
-apt intall -y libcryptsetup
-apt intall -y libdevil
-apt intall -y libdevmapper
-apt intall -y libevdev
-apt intall -y libevdev-static
-apt intall -y libgedit-tepl
-apt intall -y libimobiledevice
-apt intall -y libimobiledevice-glue
-apt intall -y libimobiledevice-glue-static
-apt intall -y libimobiledevice-static
-apt intall -y libmnl
-apt intall -y libtheora
-apt intall -y libusb
-apt intall -y libusbmuxd
-apt intall -y libusbredir
-apt intall -y libv4l
-apt intall -y libvmaf
-apt intall -y libwolfssl
-apt intall -y lit
-apt intall -y llvm-tools
-apt intall -y lvm2
-apt intall -y mate-common
-apt intall -y miniupnpc
-apt intall -y mtd-utils
-apt intall -y mtdev-static
-apt intall -y openjdk-17
-apt intall -y openjdk-21
-apt intall -y panda3d
-apt intall -y pciutils
-apt intall -y python-cryptography
-apt intall -y qt-creator
-apt intall -y qt5-qttools
-apt intall -y qt6-qttools
-apt intall -y rgbds
-apt intall -y rustc-dev
-apt intall -y scrcpy
-apt intall -y scrub
-apt intall -y teleport-tsh
-apt intall -y termux-x11-nightly
-apt intall -y usbmuxd
-apt intall -y v4l-utils
-apt intall -y wavemon
-apt intall -y xfce4-dev-tools
-apt intall -y z-push
+apt install -y bluefish
+apt install -y cuse
+apt install -y dialog
+apt install -y eww
+apt install -y gengetopt
+apt install -y getconf
+apt install -y geth
+apt install -y geth-utils
+apt install -y gettext
+apt install -y gettext-static
+apt install -y kf6-kcompletion
+apt install -y kf6-kconfigwidgets
+apt install -y kf6-ki18n
+apt install -y kf6-kjobwidgets
+apt install -y kf6-ktextwidgets
+apt install -y kf6-kwidgetsaddons
+apt install -y kf6-purpose
+apt install -y libcwidget
+apt install -y libcwidget-static
+apt install -y libfinalcut
+apt install -y libgedit-gtksourceview
+apt install -y libnewt
+apt install -y libt3widget
+apt install -y libxaw
+apt install -y libxfce4ui
+apt install -y motif
+apt install -y nzbget
+apt install -y qcustomplot
+apt install -y qtermwidget
+apt install -y qtutilities
+apt install -y rust-std-aarch64-linux-android
+apt install -y rust-std-armv7-linux-androideabi
+apt install -y rust-std-i686-linux-android
+apt install -y rust-std-wasm32-unknown-unknown
+apt install -y rust-std-wasm32-wasi
+apt install -y rust-std-wasm32-wasip1
+apt install -y rust-std-wasm32-wasip2
+apt install -y rust-std-x86-64-linux-android
+apt install -y teleport-tsh 
+apt install -y uget 
+apt install -y vegeta 
+apt install -y wget 
+apt install -y wget2
+apt install -y wget2-static 
+apt install -y wgetpaste 
+apt install -y wxwidgets 
+apt install -y xsel
+apt install -y ethtool
+apt install -y freeimage
+apt install -y frida
+apt install -y frida-dev
+apt install -y gauche
+apt install -y gecode
+apt install -y glfw
+apt install -y hz
+apt install -y i2c-tools
+apt install -y icu-devtools
+apt install -y ipmitool
+apt install -y iw
+apt install -y kf6-purpose
+apt install -y ladspa-sdk
+apt install -y libblkid
+apt install -y libccid
+apt install -y libcdk
+apt install -y libcoap
+apt install -y libcryptsetup
+apt install -y libdevil
+apt install -y libdevmapper
+apt install -y libevdev
+apt install -y libevdev-static
+apt install -y libgedit-tepl
+apt install -y libimobiledevice
+apt install -y libimobiledevice-glue
+apt install -y libimobiledevice-glue-static
+apt install -y libimobiledevice-static
+apt install -y libmnl
+apt install -y libtheora
+apt install -y libusb
+apt install -y libusbmuxd
+apt install -y libusbredir
+apt install -y libv4l
+apt install -y libvmaf
+apt install -y libwolfssl
+apt install -y lit
+apt install -y llvm-tools
+apt install -y lvm2
+apt install -y mate-common
+apt install -y miniupnpc
+apt install -y mtd-utils
+apt install -y mtdev-static
+apt install -y openjdk-17
+apt install -y openjdk-21
+apt install -y panda3d
+apt install -y pciutils
+apt install -y python-cryptography
+apt install -y qt-creator
+apt install -y qt5-qttools
+apt install -y qt6-qttools
+apt install -y rgbds
+apt install -y rustc-dev
+apt install -y scrcpy
+apt install -y scrub
+apt install -y teleport-tsh
+apt install -y termux-x11-nightly
+apt install -y usbmuxd
+apt install -y v4l-utils
+apt install -y wavemon
+apt install -y xfce4-dev-tools
+apt install -y z-push
+apt install -y termimage
+apt install -y neofetch
 pip install pyopenssl telethon requests captcha-solver
 pip install pychroot --no-deps
 pip install librosa --no-deps
+
+
+
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install wget -y 
+folder=arch-fs
+cur=`pwd`
+dlink="https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Pacman/Manjaro"
+if [ -d "$folder" ]; then
+	first=1
+	echo "skipping downloading"
+fi
+tarball="arch-rootfs.tar.gz"
+if [ "$first" != 1 ];then
+	if [ ! -f $tarball ]; then
+		echo "Download Rootfs, this may take a while base on your internet speed."
+		case `dpkg --print-architecture` in
+		aarch64)
+			archurl="aarch64" ;;
+		arm)
+			archurl="armv7" ;;
+		*)
+			echo "unknown architecture"; exit 1 ;;
+		esac
+		wget "http://os.archlinuxarm.org/os/ArchLinuxARM-${archurl}-latest.tar.gz" -O $tarball
+	fi
+	mkdir -p "$folder"
+	cd "$folder"
+	echo "Decompressing Rootfs, please be patient."
+	proot --link2symlink tar -xf ${cur}/${tarball}||:
+	cd "$cur"
+fi
+
+mkdir -p arch-binds
+mkdir -p ${cur}/${folder}/proc/fakethings
+
+if [ ! -f "${cur}/${folder}/proc/fakethings/stat" ]; then
+	cat <<- EOF > "${cur}/${folder}/proc/fakethings/stat"
+	cpu  5502487 1417100 4379831 62829678 354709 539972 363929 0 0 0
+	cpu0 611411 171363 667442 7404799 61301 253898 205544 0 0 0
+	cpu1 660993 192673 571402 7853047 39647 49434 29179 0 0 0
+	cpu2 666965 186509 576296 7853110 39012 48973 26407 0 0 0
+	cpu3 657630 183343 573805 7863627 38895 48768 26636 0 0 0
+	cpu4 620516 161440 594973 7899146 39438 47605 26467 0 0 0
+	cpu5 610849 155665 594684 7912479 40258 46870 26044 0 0 0
+	cpu6 857685 92294 387182 8096756 46609 22110 12364 0 0 0
+	cpu7 816434 273809 414043 7946709 49546 22311 11284 0 0 0
+	intr 601715486 0 0 0 0 70612466 0 2949552 0 93228 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 12862684 625329 10382717 16209 55315 8510 0 0 0 0 11 11 13 270 192 40694 95 7 0 0 0 36850 0 0 0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 286 6378 0 0 0 54 0 3239423 2575191 82725 0 0 127 0 0 0 1791277 850609 20 9076504 0 301 0 0 0 0 0 3834621 0 0 0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 806645 0 0 0 0 0 7243 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 2445850 52 1783 0 0 5091520 0 0 0 3 0 0 0 0 0 5475 0 198001 0 2 42 1289224 0 2 202483 4 0 8390 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 3563336 4202122 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 1 0 1 0 0 1 0 1 0 17948 0 0 612 0 0 0 0 2103 0 0 20 0 0 0 0 0 0 0 0 0 0 0 0 0 10 0 0 0 0 0 0 0 11 11 12 0 12 0 52 752 0 0 0 0 0 0 0 743 0 14 0 0 12 0 0 1863 229 0 464 0 0 0 0 0 0 8588 97 7236426 92766 622 31 0 0 0 18 4 4 0 5 0 0 116013 7 0 0 752406
+	ctxt 826091808
+	btime 1611513513
+	processes 288493
+	procs_running 1
+	procs_blocked 0
+	softirq 175407567 14659158 51739474 28359 5901272 8879590 0 11988166 46104015 0 36107533
+	EOF
+fi
+
+
+if [ ! -f "${cur}/${folder}/proc/fakethings/version" ]; then
+	cat <<- EOF > "${cur}/${folder}/proc/fakethings/version"
+	Linux version 5.4.0-faked (andronix@fakeandroid) (gcc version 4.9.x (Andronix fake /proc/version) ) #1 SMP PREEMPT Sun Sep 13 00:00:00 IST 2020
+	EOF
+fi
+
+if [ ! -f "${cur}/${folder}/proc/fakethings/vmstat" ]; then
+	cat <<- EOF > "${cur}/${folder}/proc/fakethings/vmstat"
+	nr_free_pages 15717
+	nr_zone_inactive_anon 87325
+	nr_zone_active_anon 259521
+	nr_zone_inactive_file 95508
+	nr_zone_active_file 57839
+	nr_zone_unevictable 58867
+	nr_zone_write_pending 0
+	nr_mlock 58867
+	nr_page_table_pages 24569
+	nr_kernel_stack 49552
+	nr_bounce 0
+	nr_zspages 80896
+	nr_free_cma 0
+	nr_inactive_anon 87325
+	nr_active_anon 259521
+	nr_inactive_file 95508
+	nr_active_file 57839
+	nr_unevictable 58867
+	nr_slab_reclaimable 17709
+	nr_slab_unreclaimable 47418
+	nr_isolated_anon 0
+	nr_isolated_file 0
+	workingset_refault 33002180
+	workingset_activate 5498395
+	workingset_restore 2354202
+	workingset_nodereclaim 140006
+	nr_anon_pages 344014
+	nr_mapped 193745
+	nr_file_pages 218441
+	nr_dirty 0
+	nr_writeback 0
+	nr_writeback_temp 0
+	nr_shmem 1880
+	nr_shmem_hugepages 0
+	nr_shmem_pmdmapped 0
+	nr_anon_transparent_hugepages 0
+	nr_unstable 0
+	nr_vmscan_write 8904094
+	nr_vmscan_immediate_reclaim 139732
+	nr_dirtied 8470080
+	nr_written 16835370
+	nr_indirectly_reclaimable 8273152
+	nr_unreclaimable_pages 130861
+	nr_dirty_threshold 31217
+	nr_dirty_background_threshold 15589
+	pgpgin 198399484
+	pgpgout 31742368
+	pgpgoutclean 45542744
+	pswpin 3843200
+	pswpout 8903884
+	pgalloc_dma 192884869
+	pgalloc_normal 190990320
+	pgalloc_movable 0
+	allocstall_dma 0
+	allocstall_normal 3197
+	allocstall_movable 1493
+	pgskip_dma 0
+	pgskip_normal 0
+	pgskip_movable 0
+	pgfree 384653565
+	pgactivate 34249517
+	pgdeactivate 44271435
+	pglazyfree 192
+	pgfault 46133667
+	pgmajfault 5568301
+	pglazyfreed 0
+	pgrefill 55909145
+	pgsteal_kswapd 58467386
+	pgsteal_direct 255950
+	pgscan_kswapd 86628315
+	pgscan_direct 415889
+	pgscan_direct_throttle 0
+	pginodesteal 18
+	slabs_scanned 31242197
+	kswapd_inodesteal 1238474
+	kswapd_low_wmark_hit_quickly 11637
+	kswapd_high_wmark_hit_quickly 5411
+	pageoutrun 32167
+	pgrotated 213328
+	drop_pagecache 0
+	drop_slab 0
+	oom_kill 0
+	pgmigrate_success 729722
+	pgmigrate_fail 450
+	compact_migrate_scanned 43510584
+	compact_free_scanned 248175096
+	compact_isolated 1494774
+	compact_stall 6
+	compact_fail 3
+	compact_success 3
+	compact_daemon_wake 9438
+	compact_daemon_migrate_scanned 43502436
+	compact_daemon_free_scanned 248107303
+	unevictable_pgs_culled 66418
+	unevictable_pgs_scanned 0
+	unevictable_pgs_rescued 8484
+	unevictable_pgs_mlocked 78830
+	unevictable_pgs_munlocked 8508
+	unevictable_pgs_cleared 11455
+	unevictable_pgs_stranded 11455
+	swap_ra 0
+	swap_ra_hit 7
+	speculative_pgfault 221449963
+	EOF
+fi
+
+bin=start-arch.sh
+echo "writing launch script"
+cat > $bin <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A arch-binds)" ]; then
+    for f in arch-binds/* ;do
+      . \$f
+    done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b /sys"
+command+=" -b /data"
+command+=" -b arch-fs/root:/dev/shm"
+command+=" -b /proc/self/fd/2:/dev/stderr"
+command+=" -b /proc/self/fd/1:/dev/stdout"
+command+=" -b /proc/self/fd/0:/dev/stdin"
+command+=" -b /dev/urandom:/dev/random"
+command+=" -b /proc/self/fd:/dev/fd"
+command+=" -b ${cur}/${folder}/proc/fakethings/stat:/proc/stat"
+command+=" -b ${cur}/${folder}/proc/fakethings/vmstat:/proc/vmstat"
+command+=" -b ${cur}/${folder}/proc/fakethings/version:/proc/version"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root" 
+command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" MOZ_FAKE_NO_SANDBOX=1"
+command+=" HOME=/root"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
+com="\$@"
+if [ -z "\$1" ];then
+    exec \$command
+else
+    \$command -c "\$com"
+fi
+EOM
+
+echo "fixing shebang of $bin"
+termux-fix-shebang $bin
+echo "making $bin executable"
+chmod +x $bin
+echo "removing image for some space"
+rm $tarball
+echo "You can now launch Arch Linux with the ./${bin} script"
+echo "Preparing additional component for the first time, please wait..."
+wget "https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/Installer/Arch/armhf/resolv.conf" -P arch-fs/root
+wget "https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/Installer/Arch/armhf/additional.sh" -P arch-fs/root
+rm -rf arch-fs/root/.bash_profile
+
+
+wget $dlink/xfce4_de.sh -O $folder/root/xfce4_de.sh
+echo " #!/bin/bash
+bash ~/additional.sh
+pacman -Syyuu --noconfirm && pacman -S wget sudo --noconfirm 
+mkdir -p ~/.vnc
+clear
+if [ ! -f /root/xfce4_de.sh ]; then
+    wget --tries=20 $dlink/xfce4_de.sh -O /root/xfce4_de.sh
+    bash ~/xfce4_de.sh
+else
+    bash ~/xfce4_de.sh
+fi
+clear
+if [ ! -f /usr/local/bin/vncserver-start ]; then
+    wget --tries=20  $dlink/XFCE4/vncserver-start -O /usr/local/bin/vncserver-start 
+    wget --tries=20 $dlink/XFCE4/vncserver-stop -O /usr/local/bin/vncserver-stop
+    chmod +x /usr/local/bin/vncserver-stop
+    chmod +x /usr/local/bin/vncserver-start
+fi
+if [ ! -f /usr/bin/vncserver ]; then
+    pacman -S tigervnc --noconfirm > /dev/null
+fi
+pacman -S firefox --noconfirm 
+clear
+echo 'Welcome to Arch Linux | XFCE'
+rm -rf ~/.bash_profile" > $folder/root/.bash_profile 
+
+bash $bin
